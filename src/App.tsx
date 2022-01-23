fa284496c5f6ef7b317406be1816b5a23dad2c2c
Daniel Salinas Merino <daniel.salinas@wondo.es>
2022-01-23 15:45:10 +0100

    feat: First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const wordChecker = (word: string, letters: string[]) => {
+  let resultWordArray: string[] = [];
+  for (let i = 0; i < word.length; i++) {
+    letters.includes(word[i])
+      ? resultWordArray.push(word[i])
+      : resultWordArray.push("*");
+  }
+  return resultWordArray;
+};
 
 function App() {
+  const word = "AhoRcado";
+  const cleanWord = word.toLowerCase();
+
+  const letters = ["e", "A", "c", "o"];
+  const cleanLetters = letters.join("").toLowerCase().split("");
+
+  const wordCheck = wordChecker(cleanWord, cleanLetters);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {wordCheck.map((wordLetter, index) => {
+        return (
+          <div
+            key={index}
+            className={`letter ${
+              !cleanLetters.includes(wordLetter) && "white"
+            }`}
+          >
+            {wordLetter.toUpperCase()}
+          </div>
+        );
+      })}
     </div>
   );
 }
